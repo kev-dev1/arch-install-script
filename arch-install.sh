4c77a37297de73c15106424846bd9b40425d2983
Kev-Dev1 <kev-dev1@users.noreply.github.com>
2021-06-16 21:07:44 +0200

    Script fertig geschrieben, aktuell in [BETA]

--- arch-install.sh
+++ arch-install.sh
@@ -14,14 +14,13 @@ echo "Wenn du damit einverstanden bist, bitte auf"
 echo "Enter drücken oder abbrechen"
 read n
 echo "Dieser Script ist auf 2 Teile gegliedert:"
-echo "Teil 'a' wird partitionierung als auch Basis eingerichtet."
-echo "Teil 'b' ist nach dem neustart."
+echo "Teil 'a' wird Partitionierung als auch die Basis eingerichtet."
+echo "Teil 'b' ist im chmod aktiv. (!Script neu starten!)"
 echo ""
 echo "Welcher Teil soll möchtest du machen? a/b"
 read teil
 echo "============================================="
-if ["$teil" == "a"]
-then
+if [[ $teil == "a" ]]; then
   echo "Die Partitionierung"
   echo ""
   lsblk
@@ -31,8 +30,7 @@ then
   echo "Hast du ein EFI oder Legacy PC? efi/legacy"
   echo "Unterschied ist: EFI modern - Legacy alt"
   read grub
-  if ["$grub" == "legacy"]
-  then
+  if [[ $grub == "legacy" ]]; then
     echo "Legacy ausgewählt"
     echo "Root Partition wird erstellt!"
     sgdisk $part -n=3:0:0
@@ -44,46 +42,50 @@ then
     lsblk
     echo "Stehen dort 2 Partitionen bei der Platte "$part" ? ja/nein"
     read ant
-    if ["$ant" == "ja"]
-      then
-        clear
-        echo "Dann machen wir weiter..."
-        echo ""
-        lsblk
-        echo "Geben sie bitte die Partition für 'root' ein! /dev/sdXX"
-        read root
-        mkfs.ext4 $root
-        mount $root /mnt
-        echo "Geben sie bitte die Partition für 'swap' ein! /dev/sdXX"
-        read swap
-        mkswap $swap
-        swapon $swap
-      elif ["$ant" == "nein"]
-        echo "Bitte starten sie den Script neu oder Partitionieren sie es selber!"
-        echo "Bei Probleme mit dem Script, melden sie es bitte in Github es!"
-      else
-        echo ""
-        echo "Tippfehler"
-      fi
+    if [[ $ant == "ja" ]]; then
+      clear
+      echo "Dann machen wir weiter..."
+      echo ""
+      lsblk
+      echo "Geben sie bitte die Partition für 'root' ein! /dev/sdXX"
+      read root
+      mkfs.ext4 $root
+      mount $root /mnt
+      echo "Geben sie bitte die Partition für 'swap' ein! /dev/sdXX"
+      read swap
+      mkswap $swap
+      swapon $swap
+      echo "Grub wird installiert..."
+      pacman -S grub
+      grub-install $root
+      break
+      grub-mkconfig -o /boot/grub/grub.cfg
+    elif [[ $ant == "nein" ]]; then
+      echo "Bitte starten sie den Script neu oder Partitionieren sie es selber!"
+      echo "Bei Probleme mit dem Script, melden sie es bitte in Github es!"
+    else
+      echo ""
+      echo "Tippfehler"
+    fi
 
-  else ["$grub" == "efi"]
-  parted $part mklabel gpt
-  echo "EFI Partition wird erstellt!"
-  sgdisk $part -n=1:0:+1024M -t=1:ef00
-  echo "SWAP Speicher wird erstellt!"
-  swapsize=$(cat /proc/meminfo | grep MemTotal | awk '{ print $2 }')
-	swapsize=$((${swapsize}/1000))"M"
-	sgdisk $part -n=2:0:+${swapsize} -t=2:8200
-  echo "Root Partition wird erstellt!"
-  sgdisk $part -n=3:0:0
-  echo ""
-  clear
-  echo "Fertig"
-  lsblk
-  echo "Stehen dort 3 Partitionen bei der Platte "$part" ? ja/nein"
-  read ant
-  if ["$ant" == "ja"]
-    then
+  elif [[ $grub == "efi" ]]; then
+    echo "EFI ausgewählt"
+    parted $part mklabel gpt
+    echo "EFI Partition wird erstellt!"
+    sgdisk $part -n=1:0:+1024M -t=1:ef00
+    echo "SWAP Speicher wird erstellt!"
+    swapsize=$(cat /proc/meminfo | grep MemTotal | awk '{ print $2 }')
+  	swapsize=$((${swapsize}/1000))"M"
+  	sgdisk $part -n=2:0:+${swapsize} -t=2:8200
+    echo "Root Partition wird erstellt!"
+    sgdisk $part -n=3:0:0
+    echo ""
+    clear
+    echo "Fertig"
+    lsblk
+    echo "Stehen dort 3 Partitionen bei der Platte "$part" ? ja/nein"
+    read ant
+    if [[ $ant == "ja" ]]; then
       clear
       echo "Dann machen wir weiter..."
       echo ""
@@ -101,33 +103,49 @@ then
       read swap
       mkswap $swap
       swapon $swap
-    elif ["$ant" == "nein"]
+      echo "Grub wird installiert..."
+      pacman -S grub efibootmgr
+      grub-install --target=x86_64-efi --efi-directory=/boot
+      break
+      grub-mkconfig -o /boot/grub/grub.cfg
+    elif [[ $ant == "nein" ]]; then
       echo "Bitte starten sie den Script neu oder Partitionieren sie es selber!"
       echo "Bei Probleme mit dem Script, melden sie es bitte in Github es!"
     else
       echo ""
       echo "Tippfehler"
     fi
+
+  pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd bash-completion wpa_supplicant netctl dialog lvm2
+  echo ""
+  echo "Hast du ein Intel oder AMD CPU verbaut? intel/amd"
+  read cpu
+  if [[ $cpu == "intel" ]]; then
+    pacman --root /mnt -S intel-ucode
+  elif [[ $cpu == "amd" ]]; then
+    pacman --root /mnt -S amd-ucode
+  else
+    echo ""
+    echo "Tippfehler"
   fi
 
-      pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd bash-completion wpa_supplicant netctl dialog lvm2
-      echo ""
-      echo "Hast du ein Intel oder AMD CPU verbaut? intel/amd"
-      read cpu
-      if ["$cpu" == "intel"]
-      then
-        pacman --root /mnt -S intel-ucode
-      elif ["$cpu" == "amd"]
-        pacman --root /mnt -S amd-ucode
-      else
-        echo ""
-        echo "Tippfehler"
-      fi
-    genfstab -Up /mnt > /mnt/etc/fstab
-    arch-chroot /mnt
+  genfstab -Up /mnt > /mnt/etc/fstab
+  arch-chroot /mnt
 
-elif ["$teil" == "b"]
-then
+elif [[ $teil == "b" ]]; then
+  echo "Wie soll dein PC heißen?"
+  read pcname
+  echo "$pcname" > etc/hostname
+  echo LANG=de_DE.UTF-8 > /etc/locale.conf
+  echo de_DE.UTF-8 > /etc/locale.gen
+  locale-gen
+  echo KEYMAP=de-latin1 > /etc/vconsole.conf
+  echo FONT=lat9w-16 >> /etc/vconsole.conf
+  ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+  pacman -Sy
+  mkinitcpio -p linux
+  echo "passwort für 'root'"
+  passwd
   echo ""
   echo "Wie soll der User heißen?"
   read user
@@ -161,4 +179,48 @@ then
   localectl set-x11-keymap de pc105 nodeadkeys
   pacman -S ttf-dejavu
   echo ""
+  clear
+  echo ""
+  echo "Jetzt wird die Desktopoberfläche installert..."
+  echo "Es gibst zur Auswahl: GNOME, KDE, XFCE(empfohlen)"
+  echo "Welche Oberfläche willst du haben? gnome/kde/xfce"
+  read deskenv
+  if [[ $deskenv == "gnome" ]]; then
+    echo "GNOME wird installert!"
+    pacman -S gnome gnome-extra gdm pipewire pipewire-pulse pipewire-alsa piewire-jack pipewire-media-session networkmanager gnome-software-packagekit-plugin flatpak
+    systemctl enable gdm
+    systemctl enable NetworkManager.service
+  elif [[ $deskenv == "kde" ]]; then
+    echo "KDE wird installert!"
+    pacman -S plasma kde-applications sddm pipewire pipewire-pulse pipewire-alsa piewire-jack pipewire-media-session networkmanager packagekit-qt5 flatpak
+    systemctl enable sddm
+    systemctl enable NetworkManager.service
+  elif [[ $deskenv == "xfce" ]]; then
+    echo "XFCE wird installert!"
+    pacman -S xfce4 xfce4-goodies lightdm networkmanager network-manager-applet lightdm-gtk-greeter lightdm-gtk-greeter-settings pipewire pipewire-pulse pipewire-alsa piewire-jack pipewire-media-session pavucontrol xfce4-whiskermenu-plugin
+    systemctl enable lightdm
+    systemctl enable NetworkManager.service
+  else
+    echo "Desktop umgebung wird nicht mit installiert."
+  fi
+  echo "Willst du auch die Standard Programme installieren (empfohlen)"
+  echo "Es beinhaltet denn Firefox(Internet), Thunderbird(Email), VLC/MPV(Multimedia)"
+  echo "Libreoffice(Office), Fonts und usw. Es erspart auch die unötigen installationen."
+  echo "ja/nein"
+  read standpro
+  if [[ $standpro == "ja" ]]; then
+    pacman -S firefox firefox-i18n-de thunderbird thunderbird-i18n-de libreoffice-fresh vlc mpv jre8-openjdk unzip git wget xz p7zip ufw iptables adobe-source-sans-pro-fonts aspell-de hunspell-de languagetool libmythes mythes-de ttf-anonymous-pro ttf-bitstream-vera ttf-dejavu ttf-droid ttf-liberation ttf-ubuntu-font-family
+  elif [[ $standpro == "nein" ]]; then
+    echo "Standard Programme werden nicht installiert!"
+  fi
+
+  echo "Du hast ArchLinux erfolgreich installiert!"
+  echo "Ich hoff dass es dir gefallen wird..."
+  echo "und Vielen Dank zur Nutzung meines Script´s"
+  echo "MFG Kev-Dev1"
+  echo "PS: Es gibt noch ein Script wo Yay installiert wird!"
+  read a
+
+else
+  echo "Tippfehler, bitte korrigieren!!"
 fi
