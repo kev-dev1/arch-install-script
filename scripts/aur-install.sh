6e51fbbb17b4458bd67d80d8fcf381af8dd408a3
Kev-Dev1 <kev-dev1@users.noreply.github.com>
2021-07-24 17:51:43 +0200

    [FIX] pre-install.sh Standard Programm aren't install

--- scripts/aur-install.sh
+++ scripts/aur-install.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-"============================================="
+echo "============================================="
 echo "GitHub:   https://github.com/kev-dev1"
 echo "Website:  https://kev-dev1.github.io"
 echo "Script:   https://github.com/kev-dev1/arch-install-script"
 echo ""
 echo "Dieser Script installiert die ArchLinux AUR-Repository"
 echo "dabei wird auch pamac installiert"
+echo "============================================="
 echo ""
-"============================================="
 echo "Yay wird installiert!"
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+echo ""
 echo "Yay ist fertig installiert!"
 rm -rf yay
 clear
 echo "Pamac wird installiert!"
 yay -S pamac-aur
+echo ""
 echo "Pamac ist fertig installiert"
 echo "Viel Spaß mit der AUR-Repository!"
 read a
